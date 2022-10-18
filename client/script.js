802720ae2b3b15c8725f33eccaaa926f76a6799a
telegrace <telegrace@gmx.de>
2022-10-18 17:31:14 +0200

    feat: uuid for userId and removable of disconnected users

--- client/script.js
+++ client/script.js
@@ -1,20 +1,23 @@
 import { createHand, removeAllChildren } from "./controlNodes.js";
 
-// remember to wrap in ()=>{}
 const socket = io();
 const playground = document.getElementById("playground");
+
+let currentSockets = [];
 
 socketListenerCurrentUsers();
 
 function socketListenerCurrentUsers() {
   socket.on("current-users", function ({ currentUsers, socketId }) {
+    currentSockets.push(socketId);
+    console.log("current sockets ", currentSockets);
     let onlineUsersCount = document.querySelector(".online-users-count");
     onlineUsersCount.innerHTML = `online: ${currentUsers}`;
 
     removeAllChildren(playground);
 
     for (let i = 0; i < currentUsers; i++) {
-      createHand(socketId);
+      createHand(socketId); //id is the same
     }
 
     const userHand = document.getElementById(socketId);
@@ -24,13 +27,24 @@ function socketListenerCurrentUsers() {
       let y = event.clientY;
 
       let width = userHand.offsetWidth;
-      let height = userHand.offsetHeight;
 
-      userHand.style.left = x - width / 2 + "px";
-      userHand.style.top = y - height / 2 + "px";
+      let userHandLeft = x - width / 2 + "px";
+      let userHandTop = y - width / 2 + "px";
+
+      userHand.style.left = userHandLeft;
+      userHand.style.top = userHandTop;
+
+      // socket.emit("hand-move", { userHandLeft, userHandTop, socketId });
     });
   });
 }
+
+socket.on("hand-move", function ({ userHandLeft, userHandTop, socketId }) {
+  // const otherHand = document.getElementById(socketId);
+  console.log("socketId ", socketId);
+  // otherHand.style.left = userHandLeft;
+  // otherHand.style.top = userHandTop;
+});
 
 document.addEventListener("click", function () {
   document.body.style.backgroundColor = "red";
--- server.ts
+++ server.ts
@@ -6,6 +6,7 @@ const { Server } = require("socket.io");
 import { Socket } from "socket.io";
 const PORT = process.env.PORT || 3000;
 const app = new Koa();
+import { v4 as uuidv4 } from "uuid";
 
 const server = http.createServer(app.callback());
 const io = new Server(server);
@@ -13,16 +14,45 @@ const io = new Server(server);
 const staticDirPath = path.join(__dirname, "client");
 app.use(serve(staticDirPath));
 
+type User = {
+  userId: string;
+  socketId: string;
+};
+
 let currentUsers = 0;
+let currentSockets: Array<User | null> = [];
 
 io.on("connection", (socket: Socket) => {
   currentUsers++;
-  let socketId = socket.id;
-  io.emit("current-users", { currentUsers, socketId });
+  const generatedUserID = uuidv4();
+  let newUser = { userId: generatedUserID, socketId: socket.id };
+  currentSockets.push(newUser);
+
+  io.emit("current-users", { currentUsers, currentSockets });
+  console.log("current-users", currentSockets);
+
   socket.on("disconnect", () => {
+    console.log("disconnect ", newUser);
+
     currentUsers--;
-    io.emit("current-users", { currentUsers, socketId });
+
+    for (let i = 0; i < currentSockets.length; i++) {
+      if (currentSockets[i]?.socketId === newUser.socketId) {
+        currentSockets.splice(i, 1);
+      }
+    }
+
+    console.log("current-users", currentSockets);
+
+    io.emit("current-users", { currentUsers, currentSockets });
+  });
+
+  socket.on("hand-move", function ({ userHandLeft, userHandTop, socketId }) {
+    console.log("socketId ", socketId);
+    socket.broadcast.emit("hand-move", { userHandLeft, userHandTop, socketId });
   });
 });
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}/ 🥳`);
+});
