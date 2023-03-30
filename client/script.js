beabdeebdb5b96338f7defd3c1f232e5bad5fac4
telegrace <telegrace@gmx.de>
2023-03-30 18:34:15 +0200

    chore: border removed and linted

--- client/script.js
+++ client/script.js
@@ -3,16 +3,16 @@ import {
   createHand,
   otherHand,
   removeHand,
-} from "./controlNodes.js";
+} from './controlNodes.js';
 
 const socket = io();
-const playground = document.getElementById("playground");
-const onlineUsersCount = document.querySelector(".online-users-count");
-const instructions = document.querySelector(".instructions");
+const playground = document.getElementById('playground');
+const onlineUsersCount = document.querySelector('.online-users-count');
+const instructions = document.querySelector('.instructions');
 
 let globalUserSocketId;
 
-socket.on("connect", () => {
+socket.on('connect', () => {
   globalUserSocketId = socket.id;
   createHand(globalUserSocketId);
   socketListenerCurrentUsers();
@@ -22,10 +22,10 @@ let socketMap = new Map();
 let elementMap = new Map();
 
 function socketListenerCurrentUsers() {
-  socket.on("user-count", function ({ userCount }) {
+  socket.on('user-count', function ({ userCount }) {
     onlineUsersCount.innerHTML = `online: ${userCount}`;
   });
-  socket.on("new-user", function ({ newUser }) {
+  socket.on('new-user', function ({ newUser }) {
     const { socketId, userId } = newUser;
     socketMap.set(socketId, userId);
     createHand(socketId);
@@ -33,35 +33,47 @@ function socketListenerCurrentUsers() {
 
   const userHand = document.getElementById(globalUserSocketId);
 
-  playground.addEventListener("mousemove", function (event) {
+  playground.addEventListener('mousemove', function (event) {
     followMouse(userHand, event);
-    userHand.style.display = "unset";
+    userHand.style.display = 'unset';
   });
-
-  // closeProximity(globalUserSocketId);
 }
 
 socket.on(
-  "other-user-hand-move",
+  'other-user-hand-move',
   function ({ userHandLeft, userHandTop, socketId }) {
     otherHand(userHandLeft, userHandTop, socketId);
     elementMap.set(socketId, { userHandLeft, userHandTop });
   }
 );
 
-socket.on("user-disconnected", function ({ userCount, socketId }) {
+socket.on('user-disconnected', function ({ userCount, socketId }) {
   socketMap.delete(socketId);
   removeHand(socketId);
   onlineUsersCount.innerHTML = `online: ${userCount}`;
 });
 
-socket.on("get-all-connections", function ({ socketMapObj }) {
+socket.on('get-all-connections', function ({ socketMapObj }) {
   const entries = Object.entries(socketMapObj);
   socketMap = new Map(entries);
-  console.log("only the new connection should see this", socketMapObj);
+  // console.log('only the new connection should see this', socketMapObj);
 
   for (const key of socketMap.keys()) {
     createHand(key);
+  }
+});
+
+socket.on('other-hand-in-range', function ({ socketId }) {
+  if (globalUserSocketId !== socketId) {
+    instructions.innerHTML = `click to high five!`;
+    changeBackgroundColor('pink');
+  }
+  confettiFunc;
+});
+
+socket.on('smackee', function ({ socketId }) {
+  if (globalUserSocketId !== socketId) {
+    new confettiFunc();
   }
 });
 
@@ -75,9 +87,9 @@ function followMouse(userHand, event) {
   let userHandLeft = mouseX - width / 2;
   let userHandTop = mouseY - width / 2;
 
-  userHand.style.left = userHandLeft + "px";
-  userHand.style.top = userHandTop + "px";
-  socket.emit("hand-move", { userHandLeft, userHandTop, globalUserSocketId });
+  userHand.style.left = userHandLeft + 'px';
+  userHand.style.top = userHandTop + 'px';
+  socket.emit('hand-move', { userHandLeft, userHandTop, globalUserSocketId });
 
   for (const key of elementMap.keys()) {
     closeProximity(key, userHandLeft, userHandTop);
@@ -96,18 +108,27 @@ function closeProximity(key, userHandLeft, userHandTop) {
     minOtherHandTop < userHandTop &&
     userHandTop < maxOtherHandTop
   ) {
+    socket.emit('hand-in-range', {
+      globalUserSocketId,
+    });
     instructions.innerHTML = `click to high five!`;
-    changeBackgroundColor("pink");
-    document.body.addEventListener("click", confettiFunc);
+    changeBackgroundColor('pink');
+
+    document.body.addEventListener('click', () => {
+      socket.emit('smacker', {
+        globalUserSocketId,
+      });
+      confettiFunc();
+    });
   } else {
     instructions.innerHTML = `move closer to a hand`;
-    changeBackgroundColor("white");
-    document.body.removeEventListener("click", confettiFunc);
+    changeBackgroundColor('white');
+    document.body.removeEventListener('click', confettiFunc);
   }
 }
 
 function toDecimal(num) {
-  let string = "0." + num.toString();
+  let string = '0.' + num.toString();
   return parseFloat(string);
 }
 
